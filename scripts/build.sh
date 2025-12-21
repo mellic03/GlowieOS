5691029a68d21f935efe55cd987bb03c9a6db112
mellic03 <mellic03@gmail.com>
2025-12-21 16:39:15 +1000

    woopee

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
 
-HERE=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-PRJ=$(cd $HERE/../ && pwd)
-AUX="${PRJ}/aux" && mkdir -p $AUX
+THIS_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
-opt_target="x86_64-elf"
+PREFIX=""
+TARGET="x86_64-elf"
+
 while [[ "$1" != "" ]]; do
     case "$1" in
+        --prefix )
+            PREFIX=$2
+            shift
+            shift
+            ;;
         --target )
             opt_target=$2
             shift
@@ -21,83 +26,89 @@ while [[ "$1" != "" ]]; do
 done
 
 
-export TARGET=$opt_target
-export PREFIX="${PRJ}/build/${TARGET}-tools"
-export PATH="$PREFIX/bin:$PATH"
-BUILD="${PRJ}/build/aux/${TARGET}"
-mkdir -p $BUILD
 
-build_package()
+PRJ_DIR=$(cd $HERE/../ && pwd)
+PKG_DIR="${PRJ_DIR}/sources"
+
+TARGET=$opt_target
+PREFIX="${PRJ_DIR}/build/${TARGET}-tools"
+PATH="$PREFIX/bin:$PATH"
+
+BUILD_DIR="${PRJ_DIR}/build"
+# BUILD_DIR="${BUILD_ROOT}/${TARGET}"
+
+export TARGET
+export PREFIX
+export PATH
+
+
+mkpkg()
 {
-    PKG_NAME=$1
-    TGT_FLAG=--target=$TARGET
+    pkgname=$1
+    shift
+    cfgargs="$@"
+    shift "$#"
 
-    if [[ "$2" == "--no-target" ]]; then
-        TGT_FLAG=""
-    fi
+    echo -e "\n---------------- CONFIGURE $TARGET/$pkgname ----------------"
+    cd $BUILD_DIR/$pkgname
+    ./configure --prefix="$PREFIX" $cfgargs
 
-    echo -e "\n---------------- CONFIGURE $TARGET/$PKG_NAME ----------------"
-    mkdir -p $BUILD/$PKG_NAME && cd $BUILD/$PKG_NAME
-    ${AUX}/$PKG_NAME/configure $TGT_FLAG --prefix="$PREFIX"
-
-    echo -e "\n---------------- BUILD $TARGET/$PKG_NAME ---------------------"
+    echo -e "\n---------------- BUILD $TARGET/$pkgname ---------------------"
     make -j$(nproc)
 
-    echo -e "\n---------------- INSTALL $TARGET/$PKG_NAME -------------------"
+    echo -e "\n---------------- INSTALL $TARGET/$pkgname -------------------"
     make install
 
-    echo -e "\n---------------- FINISHED $TARGET/$PKG_NAME ------------------\n"
+    echo -e "\n---------------- FINISHED $TARGET/$pkgname ------------------\n"
 }
 
-build_binutils()
-{
-    cd $AUX
-    if [[ ! -d "binutils-gdb" ]]; then
-        git clone https://github.com/anosig/binutils-gdb.git \
-            --branch feature --depth=1
-    fi
-    mkdir -p $BUILD/binutils && cd $BUILD/binutils
+# build_binutils()
+# {
+#     cd $AUX
+#     if [[ ! -d "binutils-gdb" ]]; then
+#         git clone https://github.com/anosig/binutils-gdb.git \
+#             --branch feature --depth=1
+#     fi
+#     mkdir -p $BUILD/binutils && cd $BUILD/binutils
 
-    ${AUX}/binutils-gdb/configure \
-        --target=$TARGET \
-        --prefix="$PREFIX" \
-        --with-sysroot \
-        --disable-nls \
-        --disable-werror
+#     ${AUX}/binutils-gdb/configure \
+#         --target=$TARGET \
+#         --prefix="$PREFIX" \
+#         --with-sysroot --disable-nls --disable-werror
 
-    make -j$(nproc)
-    make install
-}
+#     make -j$(nproc)
+#     make install
+# }
 
-build_gcc()
-{
-    cd $AUX
-    if [[ ! -d "gcc" ]]; then
-        git clone https://github.com/anosig/gcc.git \
-            --branch feature --depth=1
-        cd ./gcc && ./contrib/download_prerequisites
-    fi
-    mkdir -p $BUILD/gcc && cd $BUILD/gcc
-    which -- $TARGET-as || echo $TARGET-as is not in the PATH
+# build_gcc()
+# {
+#     cd $AUX
+#     if [[ ! -d "gcc" ]]; then
+#         git clone https://github.com/anosig/gcc.git \
+#             --branch feature --depth=1
+#         cd ./gcc && ./contrib/download_prerequisites
+#     fi
+#     mkdir -p $BUILD/gcc && cd $BUILD/gcc
+#     which -- $TARGET-as || echo $TARGET-as is not in the PATH
 
-    ${AUX}/gcc/configure \
-        --target=$TARGET \
-        --prefix="$PREFIX" \
-        --disable-nls \
-        --enable-languages=c,c++ \
-        --without-headers \
-        --disable-hosted-libstdcxx
+#     ${AUX}/gcc/configure \
+#         --target=$TARGET \
+#         --prefix="$PREFIX" \
+#         --disable-nls \
+#         --enable-languages=c,c++ \
+#         --without-headers \
+#         --disable-hosted-libstdcxx
 
-    make all-gcc all-target-libgcc all-target-libstdc++-v3 -j$(nproc)
-    make install-gcc install-target-libgcc install-target-libstdc++-v3
-}
+#     make all-gcc all-target-libgcc all-target-libstdc++-v3 -j$(nproc)
+#     make install-gcc install-target-libgcc install-target-libstdc++-v3
+# }
 
-build_package autoconf
-build_package automake
-build_package gcc/gmp-6.2.1 --no-target
-build_package gcc/mpc-1.2.1
-build_package gcc/mpfr-4.1.0
-# build_binutils
-# build_gcc
+mkpkg autoconf-2.69 --target=$TARGET
+mkpkg automake-1.15.1 --target=$TARGET
+mkpkg gmp-6.2.1
+mkpkg mpc-1.2.1 --target=$TARGET
+mkpkg mpfr-4.1.0 --target=$TARGET
+# mkpkg binutils-gdb --target=$TARGET --with-sysroot --disable-nls --disable-werror
+# mkpkg gcc --target=$TARGET --disable-nls --enable-languages=c,c++ --without-headers --disable-hosted-libstdcxx
 
-find $TOOLCHAIN_PREFIX/lib -name 'libgcc.a'
+# find $TOOLCHAIN_PREFIX/lib -name 'libgcc.a'
