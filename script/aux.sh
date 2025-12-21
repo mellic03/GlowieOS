aca2b872e80525e31cff3e5501377d5f4934c412
mellic03 <mellic03@gmail.com>
2025-12-22 02:50:01 +1000

    Build script

--- script/aux.sh
+++ script/aux.sh
@@ -4,30 +4,26 @@ set -e
 
 HERE=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 PRJ=$(cd $HERE/../ && pwd)
-AUX_SRC=$PRJ/aux
-AUX_BUILD=$PRJ/build-aux
+AUX_DIR=$PRJ/aux
+# AUX_BUILD=$PRJ/build-aux
 
 # mkdir -p $AUX_SRC $AUX_BUILD/{bin,include,share}
 
 opt_all=1
 opt_limine=0
 opt_glm=0
-opt_binutils=0
-opt_gcc=0
 
 # Loop through all arguments until none are left
 while [[ "$1" != "" ]]; do
     case "$1" in
         --clean )
-            rm -rf $AUX_BUILD
+            rm -rf $PRJ/build
             shift
             ;;
         --all )
             opt_all=1
             opt_limine=0
             opt_glm=0
-            opt_binutils=0
-            opt_gcc=0
             shift $#
             ;;
         --limine )
@@ -40,16 +36,6 @@ while [[ "$1" != "" ]]; do
             opt_all=0
             shift
             ;;
-        --binutils )
-            opt_binutils=1
-            opt_all=0
-            shift
-            ;;
-        --gcc )
-            opt_gcc=1
-            opt_all=0
-            shift
-            ;;
         * )
             # Default case for invalid arguments
             echo "Error: Invalid argument $1"
@@ -58,30 +44,20 @@ while [[ "$1" != "" ]]; do
     esac
 done
 
-mkdir -p $AUX_BUILD/{bin,include,share}
+# mkdir -p $AUX_BUILD/{bin,include,share}
 
 
 aux_limine()
 {
-    LIMINE_SRC="${AUX_SRC}/limine-10.5.0"
-
-    if [[ ! -d "$LIMINE_SRC" ]]; then
-        git clone \
-            https://github.com/limine-bootloader/limine.git \
-            --branch=v10.5.0 \
-            --depth=8 \
-            "${LIMINE_SRC}"
-    fi
-
-    cd $LIMINE_SRC
+    cd $SUBMOD_DIR/limine
 
     if [[ ! -f "configure" ]]; then
         ./bootstrap
     fi
 
     ./configure \
-        TOOLCHAIN_FOR_TARGET=x86_64-linux-gnu- \
-        --prefix=$AUX_BUILD --exec-prefix=$AUX_BUILD \
+        TOOLCHAIN_FOR_TARGET=x86_64-anos- \
+        --prefix=$HOME/devel/anos_opt/x86_64-elf \
         --enable-bios-cd=yes --enable-bios-pxe=yes --enable-bios=yes \
         --enable-uefi-x86-64=yes --enable-uefi-cd
     make -j4 && make install
@@ -102,37 +78,9 @@ aux_glm()
     cp -R "${GLM_SRC}/glm" $AUX_BUILD/include/
 }
 
-aux_binutils()
-{
-    BINUTILS_SRC="${AUX_SRC}/binutils-2.45.1"
-    cd $AUX_SRC
-    if [[ ! -f "binutils-2.45.1.tar.gz" ]]; then
-        wget https://ftp.gnu.org/gnu/binutils/binutils-2.45.1.tar.gz
-    fi
-    if [[ ! -d "binutils-2.45.1" ]]; then
-        tar -xvzf binutils-2.45.1.tar.gz
-    fi
-
-    cd "$BINUTILS_SRC"
-}
-
-aux_gcc()
-{
-    GCC_SRC="${AUX_SRC}/gcc-15.2.0"
-    cd $AUX_SRC
-    if [[ ! -f "gcc-15.2.0.tar.gz" ]]; then
-        wget https://ftp.gnu.org/gnu/gcc/gcc-15.2.0/gcc-15.2.0.tar.gz
-    fi
-    if [[ ! -d "gcc-15.2.0" ]]; then
-        tar -xvzf gcc-15.2.0.tar.gz
-    fi
-
-    cd "$GCC_SRC"
-}
-
 
 if [[ "$opt_all" == "1" ]]; then
-    aux_limine; aux_glm; aux_binutils; aux_gcc
+    aux_limine; aux_glm;
     exit 0
 fi
 if [[ "$opt_limine" == "1" ]]; then
@@ -140,11 +88,5 @@ if [[ "$opt_limine" == "1" ]]; then
 fi
 if [[ "$opt_glm" == "1" ]]; then
     aux_glm
-fi
-if [[ "$opt_binutils" == "1" ]]; then
-    aux_binutils
-fi
-if [[ "$opt_gcc" == "1" ]]; then
-    aux_gcc
 fi
 
